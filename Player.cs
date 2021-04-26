444b0c05232d726b54ce8e0d845fa9a811b1bf28
Fazer919 <78821360+Fazer919@users.noreply.github.com>
2021-04-26 16:34:34 -0700

    Player.cs
    This is the 3rd file btw if you made it to this file then you should know that this whole game is made with c#.

---
+++ Player.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Escape_the_Room
+{
+    public class Player
+    {
+        public int coins = 0;
+        public int hint = 0;
+        public int answer = 0;
+        public int h = 10;
+        
+    }
+}
